0504fdbe702bd34a27bf226af30ce2741df7897e
Balazs Gergo <gergobalazs98@gmail.com>
2023-10-18 08:42:09 +0200

    sample

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 // 1900 - 2023 
 
- const alphabet1 = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm' ]
+ /* const alphabet1 = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm' ]
 const alphabet2 =['n','o', 'p', 'q', 'r', 's', 't', 'u', 'v',  'w', 'x', 'y', 'z']
 
 let firstDigit = ['1','2']
 let secondDigit = ['9', '0']
 let thirdDigit = ['0','1','2',]
 let fourthDigit = ['0','1','2','3',]
-let customDigit = ['0','1','2','3','4','5','6','7','8','9']
-
+let customDigit = ['0','1','2','3','4','5','6','7','8','9'] */
 
  let randomYear = ""
 function yearGenerator(title) {
@@ -18,28 +17,51 @@ function yearGenerator(title) {
             randomYear += "2"
         }
 
-       if (randomYear[0] === "1" ){   // if 1.digit = 1 second digit must be 9, if starts with 2 must be 0
+       if (randomYear[0] === "1" ){   // if 1.digit = 1 second digit must be 9, if starts with 2 mustbe0,  2.digit code
         randomYear += "9"
-       } else   {
+       } else   {                 
         randomYear += "0"
-       }
-       
-       if (title[0] === "a") {
-        randomYear += "6"
-       } else if (title[0] === "b") {
-        randomYear += "7"
-       }else if (title[0] === "c") {
+       } 
+
+      if (randomYear[0] + randomYear[1] === "19" && title[1] === "n" || title[1] === "m"  ) {
         randomYear += "4"
-
-       }else   {
+      } else if (randomYear[0] + randomYear[1] === "19" && title[1] === "o"|| title[1] === "q"){
+        randomYear += "7"                                                                              // 3.digit code if starts with 19
+      }else if (randomYear[0] + randomYear[1] === "19" && title[1] === "v" || title[1] === "t") {
+        randomYear += "2"
+      } else if (randomYear[0] + randomYear[1] === "19" && title[1] === "d" || title[1] === "a" ) {
+        randomYear += "8"
+      }   else if (randomYear[0] + randomYear[1] === "20" && title[2] === "r" || title[2] === "s" ||title[2] === "t" ){ // 3 digit if strats with 20
         randomYear += "1"
-      }
-      
+      } else if (randomYear[0] + randomYear[1] === "20" && title[2] === "u" || title[2] === "e" ||  title[2] === "d"){
+        randomYear += "2"
+      }else {
+        randomYear += "0"
+      } 
 
-      
-    
+      if (randomYear[2] === "0" ) {
+        randomYear += "2"
+      } else if (randomYear[2] === "1"){
+        randomYear += "6"
+      } else if (randomYear[2] === "2") {
+        randomYear += "3"
+      } else if (randomYear[2] === "8") {           // 4.digit depends on 3.digit number
+        randomYear += "5"
+      } else if (randomYear[2] === "6") {
+        randomYear += "6"
+      } else if (randomYear[2] === "5") {
+        randomYear += "8"
+      }else {
+        randomYear += "0"
+      }
+
+      // ha az elso ketto szamjegy 19 es title b v sd f =>
+      // ha az elso ketto szamjegy 20 es title c d b g =>
 
 
        console.log(randomYear)
-}
-yearGenerator("pbsba") 
+    }
+
+       
+
+yearGenerator("terminator") 
